2cb9a9e9b35caa46e2c19468944de1d0f30328b6
PC <eli214375@gamil.com>
2023-11-24 20:35:46 +0200

    add

--- main.py
+++ main.py
@@ -15,19 +15,19 @@ st.title(title)
 
 st.image('cola.jpg', 'the cocoa cola', width=150)
 
-st.number_input("כמה קולה יש", 0)
+st.number_input("כמה קולה יש", 0,key=1)
 
 
 st.title(title)
 
 st.image('cola.jpg', 'the cocoa cola', width=150)
 
-st.number_input("כמה קולה יש", 0)
+st.number_input("כמה קולה יש", 0,key=2)
 
 
 st.title(title)
 
 st.image('cola.jpg', 'the cocoa cola', width=150)
 
-st.number_input("כמה קולה יש", 0)
+st.number_input("כמה קולה יש", 0,key=3)
 
