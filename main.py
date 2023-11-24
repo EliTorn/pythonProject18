29b4559d00768ec6517b54073f24cb77be68a688
PC <eli214375@gamil.com>
2023-11-24 20:40:16 +0200

    add

--- main.py
+++ main.py
@@ -2,32 +2,20 @@ import streamlit as st
 
 title = 'מטבחסן'
 
-
 st.title(title)
 
 st.image('cola.jpg', 'the cocoa cola', width=150)
 
 st.number_input("כמה קולה יש", 0)
 
+st.image('cola.jpg', 'the cocoa cola', width=150)
 
-
-st.title(title)
+st.number_input("כמה קולה יש", 0, key=1)
 
 st.image('cola.jpg', 'the cocoa cola', width=150)
 
-st.number_input("כמה קולה יש", 0,key=1)
-
-
-st.title(title)
+st.number_input("כמה קולה יש", 0, key=2)
 
 st.image('cola.jpg', 'the cocoa cola', width=150)
 
-st.number_input("כמה קולה יש", 0,key=2)
-
-
-st.title(title)
-
-st.image('cola.jpg', 'the cocoa cola', width=150)
-
-st.number_input("כמה קולה יש", 0,key=3)
-
+st.number_input("כמה קולה יש", 0, key=3)
